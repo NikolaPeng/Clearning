002f6766c23a60bb11777e5b916c08a9da12e8b3
pengshaogeng <pengshaogeng@gmail.com>
2022-03-15 17:25:57 +0800

    更新

--- DataStructure/LinkList3.c
+++ DataStructure/LinkList3.c
@@ -2,5 +2,35 @@
 #include <stdlib.h>
 #include <stdbool.h>
 /*
-单链表的查找
+单链表的查找：分为按位查找、按值查找
+这里只讨论有头节点的情况下
 */
+struct LNode{
+    int data;
+    struct LNode *next;
+};
+typedef struct LNode LNode, *LinkList;
+
+//1、按位查找，返回第i个元素：
+LNode *GetNode(LinkList L,int i){
+    if(i<0){
+        return NULL;
+    }
+    LNode *p;
+    int j=0;
+    p= L;
+    if(p!=NULL&&j<i-1){
+        p=p->next;
+        j++;
+    }
+    return p;
+}
+
+//2、按值查找，找到数据==e的节点
+LNode* LocateNode(LinkList L,int e){
+    LNode *p = L->next;
+    while(p!=NULL&&p->data!=e){
+        p=p->next;
+    }
+    return p;
+}
--- DataStructure/LinkList4.c
+++ DataStructure/LinkList4.c
@@ -3,4 +3,52 @@
 #include <stdbool.h>
 /*
 单链表的建立
+分为尾插法和头插法
+只研究有头节点的情况
 */
+struct LNode{
+    int data;
+    struct LNode *next;
+};
+typedef struct LNode LNode, *LinkList;
+
+//1、尾插法建立单链表
+
+//初始化一个单链表
+bool InitLink(LinkList L){
+    L = (LNode *)malloc(sizeof(struct LNode));
+    if(L == NULL){
+        return false;
+    }
+    L->next =NULL;
+    return true;
+}
+
+//在第i个元素的位置插入e
+bool LinkInsert(LinkList L,int i, int e){
+    if(i<0){
+        return false;
+    }
+    LNode *p;
+    int j=0;
+    p = L;
+    while(p!=NULL&&j<i-1){
+        p = p->next;
+        j++;
+    }
+    if(p==NULL){
+        return false;
+    }
+    LNode *s = (LNode *)malloc(sizeof(LNode));
+    s->data = e;
+    s->next = p->next;
+    p->next = s;
+    return true;
+}
+
+//尾插法
+LinkList List_TailInsert(LinkList L){
+
+}
+
+//头插法的应用：链表的逆置
