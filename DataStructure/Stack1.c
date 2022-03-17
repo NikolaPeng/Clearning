818f5c4f3b8ae6132729eeb6ddad78234ecb5251
pengshaogeng <pengshaogeng@gmail.com>
2022-03-17 09:13:05 +0800

    stack

---
+++ DataStructure/Stack1.c
@@ -0,0 +1,56 @@
+//栈（stack）：是只允许在一端进行插入和删除操作的线性表
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#define MaxSize 10
+//顺序栈的实现
+//基本操作；初始化、出栈、进栈、获取栈顶元素、判断栈空、满
+struct stack{
+    int data[MaxSize];
+    int top;
+};
+typedef struct stack sqstack;
+//初始化
+void InitStack(sqstack s){
+    s.top = -1; //  初始化栈顶指针
+}
+
+void testStack(){
+    sqstack s;
+    InitStack(s);
+    //后续的操作：增删改查等
+}
+
+//判断栈空
+bool EmptyStack(sqstack s){
+    if(s.top == -1)
+        return true;
+    else
+        return false;
+}
+
+//进栈操作
+bool push(sqstack s,int x){
+    if(s.top == MaxSize-1)
+        return false;
+    s.top = s.top + 1;
+    s.data[s.top] = x;
+    return true;
+}
+
+//出栈操作
+bool pop(sqstack s,int x){
+    if(s.top == -1)
+        return false;
+    x = s.data[s.top]; //将栈顶元素值赋给x
+    s.top = s.top - 1;
+    return true;
+}
+
+//读取栈顶元素
+bool GetTop(sqstack s,int x){
+    if(s.top == -1)
+        return false;
+    x = s.data[s.top];
+    return true;
+}
---
+++ DataStructure/Stack2.c
@@ -0,0 +1,8 @@
+//链栈:用链式存储方式实现的栈
+//基本操作；初始化、出栈、进栈、获取栈顶元素、判断栈空、满
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#define MaxSize 10
+//规定单链表只能在一端实现插入和删除的操作（头插法），将链头看作栈顶
+
